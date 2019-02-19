c959b9b411a4b5d90ac614efaec5ba31d9c00a98
Finn <finnstolze@posteo.de>
2019-02-19 08:27:09 +0100

    created a Hashrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Home from "../components/pages/Home";
-import history from "../history";
 import SecondPage from "../components/pages/SecondPage";
 
 const App = () => {
   return (
     <div>
-      <Router history={history}>
+      <HashRouter>
         <div>
           <Route path={"/"} exact component={Home} />
           <Route path={"/secondpage"} component={SecondPage} />
         </div>
-      </Router>
+      </HashRouter>
     </div>
   );
 };
