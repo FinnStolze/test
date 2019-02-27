54a5ae0f01208c5a4064fbfbb295c21ccbaab63e
Finn <finnstolze@posteo.de>
2019-02-27 16:20:08 +0100

    Updates

---
+++ precache-manifest.3177914c567dd6eeab9e4facefd58086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ee684aeb3d48f84a5ac",
+    "url": "/test/static/css/main.d52018f4.chunk.css"
+  },
+  {
+    "revision": "3ee684aeb3d48f84a5ac",
+    "url": "/test/static/js/main.3ee684ae.chunk.js"
+  },
+  {
+    "revision": "a03dc12cda14a16444a4",
+    "url": "/test/static/js/runtime~main.a03dc12c.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/test/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "60383b2bc1fc8c92ba07dd914b62b508",
+    "url": "/test/index.html"
+  }
+];
